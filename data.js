eed3fccca87ccb64c1a8ab2412b891b04a90f6e1
Chandra2407 <chandra240797@gmail.com>
2022-03-23 14:44:43 +0530

    Music Player

--- data.js
+++ data.js
@@ -8,7 +8,27 @@ const admin = [
         password:"selena3003"
     },
 ]
-
+const MusicList = [
+    {
+        img:"img/FairyTail.jpg",
+        name:"Fairy Tail Opening",
+        artist:"Fairy Tail",
+        music:"music/FairyTail.mp3"
+    },
+    {
+        img:"img/TokyoGhoul.jpg",
+        name:"TokyoGhoul-OST-Licht-und-Schatten",
+        artist:"Tokyo Ghoul",
+        music:"music/TokyoghoulOST-Licht-und-Schatten.m4a",
+    },
+    {
+        img:"img/TokyoGhoul.jpg",
+        name:"TokyoGhoul-Unravel",
+        artist:"Tokyo Ghoul",
+        music:"music/TokyoGhoul-Unravel.m4a",
+    }
+]
 export {
-    admin
+    admin,
+    MusicList
 };
